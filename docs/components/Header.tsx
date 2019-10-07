c8c0f835fac9e3ec4271990a7160077e3708bbff
stan <stanislav@yarmonov.pro>
2019-10-07 19:29:48 +0300

    +Icons

--- docs/components/Header.tsx
+++ docs/components/Header.tsx
@@ -1,25 +1,19 @@
 import Link from "next/link";
-import { useState } from "react";
+import GitHub from "../components/icons/GitHub";
 
-function Header() {
-    const [isExpanded, toggleExpansion] = useState(false);
-
-    return (
-        <header className="border-black border-solid border-b-2 bg-gray-200">
-            <div className="flex flex-wrap md:flex-no-wrap items-center justify-between max-w-4xl mx-auto p-4 md:p-6">
-                <div className="flex items-center">
-                    <Link href="/">
-                        <a className="font-bold text-xl">SDE Team</a>
-                    </Link>
-                </div>
-                <div>
-                    <a href="https://github.com/StanEgo/sde-team" className="font-bold">
-                        GitHub
-                    </a>
-                </div>
+export default () => (
+    <header className="border-black border-solid border-b-2 bg-gray-200">
+        <div className="flex flex-wrap md:flex-no-wrap items-center justify-between max-w-4xl mx-auto p-4 md:p-6">
+            <div className="flex items-center">
+                <Link href="/">
+                    <a className="font-bold text-xl">SDE Team</a>
+                </Link>
             </div>
-        </header>
-    );
-}
-
-export default Header;
+            <div>
+                <a href="https://github.com/StanEgo/sde-team" className="font-bold">
+                    <GitHub />
+                </a>
+            </div>
+        </div>
+    </header>
+);
