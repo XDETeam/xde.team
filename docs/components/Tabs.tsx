ccdcb762588bb253745f9e780fddc9dd21924482
stan <stanislav@yarmonov.pro>
2019-10-14 19:29:36 +0300

    +Tabs control for docs.

---
+++ docs/components/Tabs.tsx
@@ -0,0 +1,35 @@
+import { FC, SFCElement, useState, ReactElement } from "react";
+
+interface ITabProps {
+    caption: string;
+    children?: ReactElement;
+}
+
+interface ITabsProps {
+    children?: SFCElement<ITabProps>[];
+}
+
+const Tabs: FC<ITabsProps> = ({ children }) => {
+    const [index, setIndex] = useState(0);
+
+    if (!children) {
+        return null;
+    }
+
+    return (
+        <nav className="tabs">
+            <div className="header">
+                { children.map((tab, i) => (
+                    <div className={ `tab${index === i ? ' active' : ''}` }>
+                        <button onClick={ () => setIndex(i) }>{ tab.props.caption }</button>
+                    </div>
+                ))}
+            </div>
+            <div className="p-4 bg-gray-800 text-white">
+                { children[index].props.children }
+            </div>
+        </nav>
+    );
+}
+
+export { Tabs };
