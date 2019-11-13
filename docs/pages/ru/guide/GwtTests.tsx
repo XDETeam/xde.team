9c1affa22d74ce10eeda2748c126cece6bb4eb49
stan <stanislav@yarmonov.pro>
2019-11-13 23:57:31 +0200

    *Specification experiments.

--- docs/pages/ru/guide/GwtTests.tsx
+++ docs/pages/ru/guide/GwtTests.tsx
@@ -3,19 +3,19 @@ import { Alice } from "@sde/team-specs";
 
 export interface IEntity {}
 
-export interface ICommand<TRequest, TResponse> {
+export type Command<TRequest, TResponse> = {
 	execute(request: TRequest): TResponse;
-}
+};
 
-export interface IDeployCommand<TEntity extends IEntity> extends ICommand<TEntity, void> {}
+export type DeployCommand<TEntity extends IEntity> = Command<TEntity, void>;
 
-export class DeployCommand<TEntity> implements IDeployCommand<TEntity> {
+export class PgDeployCommand<TEntity> implements DeployCommand<TEntity> {
 	execute(request: TEntity): void {
 		console.log("Deployed successfully");
 	}
 }
 
-const exists = new DeployCommand();
+const exists = new PgDeployCommand();
 
 export type SignInRequest = {
 	eMail: string;
@@ -26,7 +26,7 @@ export type SignInResponse = {
 	exists: boolean;
 };
 
-export class SignInCommand implements ICommand<SignInRequest, SignInResponse> {
+export class SignInCommand implements Command<SignInRequest, SignInResponse> {
 	execute(request: SignInRequest): SignInResponse {
 		console.log("SignInCommand call");
 
@@ -38,19 +38,35 @@ export class SignInCommand implements ICommand<SignInRequest, SignInResponse> {
 
 const signIn = new SignInCommand();
 
-export type Specification = {};
+export type GivenSpecification = {};
 
 function given<TRequest, TResponse>(
 	literals: TemplateStringsArray,
 	subject: TRequest,
-	command: ICommand<Partial<TRequest>, TResponse>
-): Specification {
+	command: Command<Partial<TRequest>, TResponse>
+): GivenSpecification {
 	console.log("subject", subject);
 	console.log("result", command.execute(subject));
 	console.log("command", command);
 
 	return {}; // TODO:
 }
+
+type Exception = {};
+
+export type Specification<TRequest, TResponse> = {
+	given: () => TRequest;
+	when: (request: TRequest) => TResponse;
+	then: (response: TResponse | Exception) => void;
+};
+
+const SignInSpecification: Specification<SignInRequest, SignInResponse> = {
+	given: () => ({ eMail: "alice@domain.com", password: "!qa2Ws3eD" }),
+	when: request => ({ exists: request.eMail == "alice@domain.com" }),
+	then: response => {}
+};
+
+//export function Test<SignInSpecification>(Alice)
 
 export const test1 = given`${Alice} ${exists}`;
 export const test2 = given`${Alice} ${signIn}`;
@@ -62,6 +78,9 @@ export const tests = `
 		- За предикат
 			- Иногда может быть один предикат, без субъекта.
 			- Вроде как предикаты впереди - это мощно
+	- Tagged templates можно организовать на conditional types
+		type BaseFunc = (a: string) => Promise<string>
+		type BaseWithB  = BaseFunc extends (...a: infer U) => infer R ? (b: number, ...a:U) => R: never;	
 
 	Given Exists Alice # Given Alice sample
 	When SignIn Alice
