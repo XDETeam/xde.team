4516008ba3a4eb2487a6d125363cf0ac5f17f6b7
yavulan <yavulan@gmail.com>
2019-11-13 20:44:28 +0200

    + Added stylelint
    - Fixed <p> margin

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-    plugins: [require("tailwindcss"), require("autoprefixer"), require("postcss-nested")]
+    plugins: [
+        require("stylelint"),
+        require("tailwindcss"),
+        require("autoprefixer"),
+        require("postcss-nested")
+    ]
 };
