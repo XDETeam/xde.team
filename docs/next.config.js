2cf98e98423953dfd17de7bc9396e32e3bd21321
stan <stanislav@yarmonov.pro>
2020-05-08 10:10:37 +0300

    *Plugins composer removed.

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,7 +1,5 @@
 const path = require("path");
 
-const withPlugins = require("next-compose-plugins");
-
 const css = require("@zeit/next-css");
 const images = require("remark-images");
 const emoji = require("remark-emoji");
@@ -27,25 +25,21 @@ const nextConfig = {
     }
 };
 
-module.exports = withPlugins(
-    [
-        [
-            css,
-            {
-                // This settings required to import CSS files from packages.
-                // For example: import "katex/dist/katex.css"
-                cssLoaderOptions: {
-                    url: false
-                }
-            }
-        ],
+module.exports = [
+    css,
+    config => ({
+        ...config,
+        // This settings required to import CSS files from packages.
+        // For example: import "katex/dist/katex.css"
+        cssLoaderOptions: {
+            url: false
+        }
+    }),
 
-        [
-            mdx,
-            {
-                pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"]
-            }
-        ]
-    ],
-    nextConfig
-);
+    mdx,
+    config => ({
+        ...config, pageExtensions: {
+            url: false
+        }
+    }),
+]
