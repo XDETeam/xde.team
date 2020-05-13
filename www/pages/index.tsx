eb54c5241f548c37afae32dcb3337ef3bd6d6d02
stan <stanislav@yarmonov.pro>
2020-05-13 18:03:16 +0300

    +Vision.

--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -1,18 +1,21 @@
 import Link from "next/link";
 
 export const Page = () => (
-	<>
-		<h1>SDE.Team: Deploying...</h1>
+    <>
+        <h1>SDE.Team</h1>
 
-		<ul>
-			<li>
-				<Link href="/spec">Specs</Link>
-			</li>
-			<li>
-				<Link href="/app">Apps</Link>
-			</li>
-		</ul>
-	</>
+        <ul>
+            <li>
+                <Link href="/vision">Vision</Link>
+            </li>
+            <li>
+                <Link href="/spec">Specs</Link>
+            </li>
+            <li>
+                <Link href="/app">Apps</Link>
+            </li>
+        </ul>
+    </>
 );
 
 export default Page;
---
+++ www/pages/vision/index.tsx
@@ -0,0 +1,16 @@
+export const Page = () => (
+    <>
+        <h1>Vision</h1>
+
+        <h2>Team</h2>
+        <p>SDE is a team of people who loves to create.</p>
+
+        <h2>Toolkit</h2>
+        <p>
+            SDE is a toolkit to create something amazing. No matter what you
+            want to create. Even if this is a new science or a cooking recipe.
+        </p>
+    </>
+);
+
+export default Page;
