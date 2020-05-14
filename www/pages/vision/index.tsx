8e68e8a03c69b23f1d7efe3a6d9d7303d53d728e
stan <stanislav@yarmonov.pro>
2020-05-14 20:15:31 +0300

    *Vision.

--- www/pages/vision/index.tsx
+++ www/pages/vision/index.tsx
@@ -2,16 +2,23 @@ export const Page = () => (
     <>
         <h1>Vision</h1>
 
-        <h2>Team</h2>
+        <h2>Engineering</h2>
+        <p>Engineering is a:</p>
+
+        <h3>Team</h3>
         <p>
             SDE is a team of people who loves to create. Especially something
             awesome and unusual.
         </p>
 
-        <h2>Toolkit</h2>
+        <h3>Tools</h3>
         <p>
             SDE is a toolkit to create something amazing. No matter what you
             want to create. Even if this is a new science or a cooking recipe.
+        </p>
+        <p>
+            Primary tools are methodological (know-how). They are always with
+            us.
         </p>
     </>
 );
