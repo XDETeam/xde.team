b7eabdbe4fe7a98ac785e40ccdb4eae24ddfb626
stan <stanislav@yarmonov.pro>
2020-05-16 21:43:12 +0300

    +Ansible specs.

---
+++ www/pages/spec/ansible-automation.tsx
@@ -0,0 +1,49 @@
+export const Page = () => (
+    <>
+        <h1>Ansible</h1>
+
+        <p>
+            Introduce <a>host</a> term?
+        </p>
+
+        <p>
+            Some reusable <a>SVG</a> picture with schema and flow would be nice.
+        </p>
+
+        <h2>Control node</h2>
+        <p>Machine with Ansible installed to run commands and playbooks.</p>
+        <p>
+            Conside{" "}
+            <a href="https://github.com/cytopia/docker-ansible">Docker image</a>{" "}
+            to run the control node quickly.
+        </p>
+        <p>Typical management pattern?</p>
+
+        <h2>Managed nodes</h2>
+        <p>Hosts to be managed.</p>
+
+        <h2>Inventory</h2>
+        <p>
+            A list (structure?) of <a>managed nodes</a>.
+        </p>
+
+        <h2>Modules</h2>
+        <p>
+            The unit of execution containing a specific <a>responsibility</a>.
+        </p>
+
+        <h2>Tasks</h2>
+        <p>
+            Probably <a>module</a> can be considered as function with parameters
+            when <a>task</a> is its call?
+        </p>
+
+        <h2>Playbooks</h2>
+        <p>
+            Ordered list of <a>tasks</a> applied for hosts from <a>inventory</a>
+            .
+        </p>
+    </>
+);
+
+export default Page;
--- www/pages/spec/devops.tsx
+++ www/pages/spec/devops.tsx
@@ -1,6 +1,16 @@
+import Link from "next/link";
+
 export const Page = () => (
     <>
         <h1>DevOps</h1>
+
+        <ul>
+            <li>
+                <Link href="/spec/ansible-automation">
+                    <a>Ansible</a>
+                </Link>
+            </li>
+        </ul>
     </>
 );
 
