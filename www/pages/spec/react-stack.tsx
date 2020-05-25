927f50319cab95e6587f205a829cea3c4fa06710
stan <stanislav@yarmonov.pro>
2020-05-25 22:42:22 +0300

    *React notes.

--- www/pages/spec/react-stack.tsx
+++ www/pages/spec/react-stack.tsx
@@ -1,5 +1,10 @@
 export const Page = () => <>
     <h1>React</h1>
+
+    <h2>Parent-child communications</h2>
+    <p>Passing props (props-drilling?), passing callback in props.</p>
+    <p>Context (including useContext hooks)</p>
+    <p>Redux</p>
 </>
 
 export default Page;
