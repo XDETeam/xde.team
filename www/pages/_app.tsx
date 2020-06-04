b398cbeac151adb4e7e84c26144848938678f3af
stan <stanislav@yarmonov.pro>
2020-06-04 21:09:16 +0300

    +Alpha warning.

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -1,9 +1,10 @@
-import Toolbar from "../ui/toolbar";
-
 import "../ui/styles.css";
+import Toolbar from "../ui/toolbar";
+import AlphaAlert from "../ui/alpha-alert";
 
 export const Page = ({ Component, pageProps }) => (
     <>
+        <AlphaAlert />
         <Component {...pageProps} />
         <Toolbar />
     </>
---
+++ www/ui/alpha-alert.tsx
@@ -0,0 +1,9 @@
+import styles from "./alpha-alert.module.css";
+
+export const AlphaAlert = () => (
+    <div className={styles.alert}>
+        We are under very early construction, so please indulge us.
+    </div>
+);
+
+export default AlphaAlert;
