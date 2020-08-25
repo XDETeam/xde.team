7d307283c76a5970a76e713adfd535de09a3e6c1
stan <stanislav@yarmonov.pro>
2020-08-25 19:09:51 +0300

    +Basic layout.

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -4,6 +4,112 @@ import { Layout } from "../templates/layout";
 export const Home = () => (
 	<Layout>
 		<h1>XDE</h1>
+
+		<section>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam ducimus in sint odio
+				saepe maiores cupiditate, commodi assumenda. Cupiditate laudantium labore et harum
+				minus, voluptatem deserunt eaque suscipit officia consequuntur. Praesentium, vitae
+				neque. A soluta cupiditate odio sit nemo reiciendis. Facere nisi magni
+				reprehenderit, laudantium molestiae ullam voluptas amet veniam, perspiciatis
+				veritatis porro pariatur dolore, ad sint similique itaque! Eius. Dicta debitis
+				officia cupiditate inventore quis ab, distinctio eveniet? Tempora vel rem error
+				facere. Possimus, praesentium optio architecto ipsa accusantium corporis aperiam,
+				impedit, sint commodi ex incidunt saepe dolore deserunt. Quasi cumque facilis
+				voluptate perspiciatis, magni, vel suscipit voluptatem unde consequuntur aliquid
+				deleniti in culpa, voluptas magnam! Repellat magni quos quidem modi vitae quo
+				laborum explicabo enim, ad amet tempore! Nam sapiente sunt expedita vel, quod minus
+				molestiae velit dignissimos, nesciunt temporibus suscipit sit similique incidunt id
+				quibusdam obcaecati recusandae voluptate delectus iure cupiditate repudiandae
+				doloremque? Pariatur natus fugiat ipsa. Nobis voluptates eaque maiores totam ratione
+				sunt, commodi ad architecto magni.
+			</p>
+			<p>
+				Vel perferendis eligendi ad architecto, velit iure incidunt! Necessitatibus quaerat
+				facilis commodi ea eaque odit quos velit aliquam eius. Harum doloremque cumque,
+				quaerat, sapiente obcaecati beatae quam, autem voluptatem expedita id tempora animi
+				dicta cupiditate impedit nobis veniam sit consequuntur neque eos dignissimos quae
+				vel ab molestiae fugiat.
+			</p>
+			<p>
+				Soluta! Minima molestias qui dolores facilis rerum quasi laudantium ea, mollitia
+				fugiat? Veniam ipsam qui aliquam deleniti eveniet adipisci consequatur vel, soluta
+				autem tempora? Doloremque, excepturi fuga totam molestiae debitis beatae? Architecto
+				quidem quasi aliquid praesentium fuga recusandae ad vel saepe hic repellat omnis,
+				perspiciatis veritatis odit quae ex fugiat debitis assumenda, ratione, expedita
+				iusto. Possimus saepe tempore accusamus culpa magni. Ad illum voluptas unde
+				accusamus, eius molestias iure laborum expedita recusandae ullam adipisci non rem
+				optio nesciunt dignissimos ipsum odio? Aperiam quibusdam voluptatem, odit aliquam
+				nihil totam aliquid autem excepturi. Vel dicta exercitationem asperiores illum,
+				nostrum expedita quaerat doloremque maiores sunt id, animi facilis vero eos amet,
+				est nam maxime possimus nihil facere. Quis commodi recusandae hic sapiente
+				laboriosam incidunt! Reprehenderit tempora impedit similique maxime perspiciatis,
+				perferendis repellendus accusamus a, repellat voluptatibus quam, maiores dolorem id.
+				Repellendus exercitationem deserunt culpa laudantium impedit voluptas beatae eos
+				suscipit. Accusantium itaque optio aliquid. Ipsa amet ut molestias dignissimos
+				quidem voluptas impedit odio a quod consequuntur. Obcaecati nam reiciendis quam iste
+				ipsum, delectus vero praesentium officiis? Ducimus fuga modi aspernatur distinctio
+				voluptate dolorem molestias. Aut, eligendi quidem? Natus similique libero
+				dignissimos harum, architecto repellendus cumque sit obcaecati ea deleniti eius
+				repudiandae ut sapiente veniam! Voluptatibus molestias doloribus aspernatur qui at
+				ipsam dolorem neque fugit! Perspiciatis odio quo libero eaque! Necessitatibus
+				dolorum non pariatur eos facilis in, velit consequuntur expedita recusandae
+				voluptate, rerum, asperiores itaque cupiditate corrupti ratione!
+			</p>
+			<p>
+				Facere ut minus nesciunt, veniam tempora eos? Nulla dolor soluta optio numquam?
+				Adipisci et, sequi optio sit consequatur facere modi officia deserunt nihil quam
+				nobis ratione dolore harum quae rerum inventore quidem eveniet, reiciendis
+				cupiditate animi. Totam. Culpa aspernatur impedit iusto nemo autem ea minus
+				laboriosam quo vero. Ullam excepturi aspernatur porro fugiat aut eaque voluptate
+				voluptatum consequuntur molestias hic enim, fuga inventore, recusandae, quidem nemo
+				illum. Tenetur nihil quibusdam laborum enim beatae autem atque vitae fugiat
+				consequatur modi adipisci, consequuntur voluptatibus officia dolor similique illo
+				quod. Iusto animi quae molestias debitis rem non commodi cupiditate repellendus.
+				Consequuntur minima hic numquam vero, dicta obcaecati, error totam cumque incidunt
+				repellat officiis temporibus quisquam iste, accusamus quis eaque. Pariatur ab
+				obcaecati quod velit illum eligendi eum, aut voluptatem fugiat. Nobis inventore
+				ratione aut, harum voluptate enim rerum minus sed iusto! Odio eaque hic praesentium
+				quia impedit voluptatem aperiam eveniet molestiae nostrum perspiciatis dignissimos
+				reprehenderit suscipit nobis, voluptates dicta odit? Perferendis, ratione. Cumque
+				temporibus vitae enim doloremque recusandae veniam iusto, maiores atque aperiam
+				molestias asperiores eum qui deserunt natus ipsa corporis reiciendis? Enim itaque
+				officiis dolores ex, quo possimus. Maxime? Dolorum asperiores expedita iusto.
+				Maiores quaerat soluta velit, at nisi magni in voluptates minus sit aut commodi
+				nulla ratione aspernatur? Eligendi et qui animi a vitae dicta, vel harum sequi!
+				Suscipit, alias iure laborum nihil sequi odio repellendus rerum, cum molestias natus
+				commodi! Quisquam, molestiae dolores tenetur voluptate facere neque consequuntur
+				excepturi quod impedit magnam aliquam inventore perspiciatis vitae quaerat.
+			</p>
+			<p>
+				Commodi temporibus provident ratione, eaque quas nobis recusandae quaerat aliquam
+				minus officiis aperiam quos porro perspiciatis, quia exercitationem dolor beatae
+				dignissimos nulla ipsa voluptates autem ut assumenda labore culpa! Eveniet. Dolores
+				expedita voluptates sint illo. Minima vero itaque ducimus atque numquam. Similique
+				dicta tempore totam natus vitae recusandae dolores nulla in quasi deleniti, numquam
+				velit quaerat id incidunt saepe nemo! Deleniti deserunt reiciendis quae vitae
+				recusandae alias provident nemo unde, rerum impedit est ullam sequi nam, amet ipsam,
+				perspiciatis assumenda necessitatibus atque dicta quis quibusdam ducimus molestias
+				enim. Impedit, cumque. Ipsam voluptatem ducimus delectus soluta tenetur dolorum
+				excepturi odit cupiditate, mollitia, veritatis quibusdam sit consequatur illum
+				quidem, saepe amet aliquid ea et exercitationem nihil reprehenderit architecto
+				laudantium perferendis. Accusantium, tempora. Aut veritatis, eligendi similique
+				delectus modi dolore blanditiis deserunt, aperiam repudiandae voluptatum magni
+				adipisci omnis. Quo perspiciatis vitae error eligendi sint nihil mollitia hic
+				consectetur dolores eum, cumque sunt numquam. Animi consequatur aut saepe magni
+				officia. Recusandae sapiente sint nam numquam architecto asperiores consequatur at
+				amet beatae ipsam cupiditate fugit debitis porro, illo, eos earum consequuntur
+				labore tempore! Sapiente, atque. Doloribus dignissimos at laborum aliquam ad ut,
+				atque corrupti delectus ex molestias a ducimus. Totam veritatis architecto autem aut
+				quo est sunt, sapiente soluta tenetur neque officiis beatae ratione facilis. Tempore
+				soluta sapiente aliquam molestias maiores harum perferendis exercitationem optio
+				aliquid modi repudiandae expedita quam, eveniet eius minus ullam voluptatem aperiam
+				odio quos magnam laboriosam omnis ab. Possimus, rerum similique. Adipisci rerum hic
+				quod repellat, deserunt ipsam alias accusantium dicta officiis modi magni eos, nam
+				commodi unde atque dolorem saepe? Facilis vero eum veritatis qui nesciunt vitae
+				reiciendis minus explicabo?
+			</p>
+		</section>
 	</Layout>
 );
 
--- www/src/templates/layout.tsx
+++ www/src/templates/layout.tsx
@@ -3,8 +3,12 @@ import styles from "./layout.module.css";
 
 export const Layout: FC = ({ children }) => (
 	<>
-		<header></header>
 		<main className={styles.main}>{children}</main>
-		<footer></footer>
+		<footer className={styles.footer}>
+			<div>Footer 1</div>
+			<div>Footer 2</div>
+			<div>Footer 3</div>
+		</footer>
+		<aside className={styles.aside}>X</aside>
 	</>
 );
