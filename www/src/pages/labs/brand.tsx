75e4674c13bb8d8a041907583921dabd78042ed5
stan <stanislav@yarmonov.pro>
2020-08-26 10:14:00 +0300

    +Brand lab.

---
+++ www/src/pages/labs/brand.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Logo = () => <svg>TODO:</svg>;
+
+export const Brand = () => (
+	<>
+		<p>Форма повествования может быть сформулирована как инжиниринг бренда</p>
+		<p>x-Driven Engineering</p>
+		<p>"X" символизирует ген, а тот - создание (generate, generation, etc.)</p>
+	</>
+);
+
+export default Brand;
