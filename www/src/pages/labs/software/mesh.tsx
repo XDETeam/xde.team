051527d978199d7a2deb8e4ae19ec49a47967673
stan <stanislav@yarmonov.pro>
2020-08-27 22:16:59 +0300

    +Mesh lab.

---
+++ www/src/pages/labs/software/mesh.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Layout } from "../../../templates/layout";
+
+export const Mesh = () => (
+    <Layout>
+        <header>
+            <h1>Mesh</h1>
+        </header>
+
+        <section>
+            <p>
+                Хорошо идею mesh озвучивает словарь Collins: "If two things or ideas mesh or are
+                meshed, they go together well or fit together closely."
+            </p>
+
+            <p>
+                В современном Software Engineering одной из основных проблем является реакция
+                существующих систем на изменения и экспоненциальный рост их сложности. Это
+                становится основной болью. Mesh - это такая организация данных (а значит и кода
+                <a href="program-and-or-data">Program and/or data</a>), когда новые решения не
+                только не создают проблем, а усиливают те решения, что были.
+			</p>
+
+            <p>
+                Стоит привести пример реализации семантической сети в СУБД, которую в своё время
+                реализовывали.
+            </p>
+        </section>
+    </Layout>
+);
+
+export default Mesh;
