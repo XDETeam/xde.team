ab5d4548104f09e53b3a6cd71914989de5c63655
stan <stanislav@yarmonov.pro>
2020-08-27 21:51:32 +0300

    +Vision.

---
+++ www/src/pages/labs/xde/vision.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Layout } from "../../../templates/layout";
+
+export const Vision = () => (
+    <Layout>
+        <header>
+            <h1>Vision</h1>
+        </header>
+
+        <section>
+            <p>
+                Всё сущее вокруг нас можно разделить на натуральное и искуственное. Первое
+                возникает без нашего в нём участия. Искусственное - это наши инженерные
+                творения. И чем больше в нём новизны (engineer &rarr; ingenius &rarr; genius
+                - "one who produces or generates [new] things"), тем сильнее нам хочется
+                назвать это искусством. В английском языке artificial и art сохраняет те же
+                принципы.
+            </p>
+
+            <p>
+                XDE.Team объединяет людей, любящих создавать, ориентируясь именно на это
+                качество, а не на область применения.
+            </p>
+        </section>
+    </Layout>
+);
+
+export default Vision;
