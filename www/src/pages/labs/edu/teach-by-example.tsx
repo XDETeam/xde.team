8ec1bdfc1b9c9ee94ef81eb1c8f6672919c317d2
yavulan <yavulan@gmail.com>
2020-09-01 11:41:47 +0300

    +Added teach by example

---
+++ www/src/pages/labs/edu/teach-by-example.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Layout } from "../../../templates/layout";
+
+export const TeachByExample = () => (
+	<Layout>
+		<header>
+			<h1>Teach by example</h1>
+		</header>
+
+		<section>
+			<p>Обучение должно опираться на разносторонние примеры.</p>
+			<p>Наилучший способ обучить программам поведения - своим примером.</p>
+		</section>
+	</Layout>
+);
+
+export default TeachByExample;
