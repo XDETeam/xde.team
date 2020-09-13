0a324b6b523ca1eb82b04c88d3231742102c31fb
stan <stanislav@yarmonov.pro>
2020-09-13 17:46:53 +0300

    *Legacy Gatsby sample removed.

--- labs/gatsby-www/lib/Is.tsx
+++
@@ -1,3 +0,0 @@
-import React, { FC } from "react";
-
-export const Is: FC<{ a: string }> = (props) => <div>Is a {props.a}</div>;
--- labs/gatsby-www/mesh/Squat.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Squat = () => <h1>Squat</h1>;
-
-export default Squat;
