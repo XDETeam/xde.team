e3efde18a1c2ea78e340d1250fc6e622399a10d4
stan <stanislav@yarmonov.pro>
2020-09-13 16:11:11 +0300

    *Polishing.

--- www/src/pages/labs/software/mesh.tsx
+++ www/src/pages/labs/software/mesh.tsx
@@ -2,31 +2,40 @@ import React from "react";
 import { Layout } from "../../../templates/layout";
 
 export const Mesh = () => (
-    <Layout>
-        <header>
-            <h1>Mesh</h1>
-        </header>
+	<Layout>
+		<header>
+			<h1>Mesh</h1>
+		</header>
 
-        <section>
-            <p>
-                Хорошо идею mesh озвучивает словарь Collins: "If two things or ideas mesh or are
-                meshed, they go together well or fit together closely."
-            </p>
-
-            <p>
-                В современном Software Engineering одной из основных проблем является реакция
-                существующих систем на изменения и экспоненциальный рост их сложности. Это
-                становится основной болью. Mesh - это такая организация данных (а значит и кода
-                <a href="program-and-or-data">Program and/or data</a>), когда новые решения не
-                только не создают проблем, а усиливают те решения, что были.
+		<section>
+			<p>
+				Хорошо идею mesh озвучивает словарь Collins: "If two things or ideas mesh or are
+				meshed, they go together well or fit together closely."
 			</p>
 
-            <p>
-                Стоит привести пример реализации семантической сети в СУБД, которую в своё время
-                реализовывали.
-            </p>
-        </section>
-    </Layout>
+			<p>
+				В современном Software Engineering одной из основных проблем является реакция
+				существующих систем на изменения и экспоненциальный рост их сложности. Это
+				становится основной болью. Mesh - это такая организация данных (а значит и кода
+				<a href="program-and-or-data">Program and/or data</a>), когда новые решения не
+				только не создают проблем, а усиливают те решения, что были.
+			</p>
+		</section>
+
+		<section>
+			<h2>InBox</h2>
+
+			<p>
+				Стоит привести пример реализации семантической сети в СУБД, которую в своё время
+				реализовывали.
+			</p>
+
+			<p>
+				Почему важно любить то, чем занимаешься, того с кем дружишь и с кем живешь - это
+				прочные связи.
+			</p>
+		</section>
+	</Layout>
 );
 
 export default Mesh;
