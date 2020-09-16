ed5140777077b133909edada4f68f16da91dd997
stan <stanislav@yarmonov.pro>
2020-09-16 10:44:01 +0300

    *XDE info refactoring.

---
+++ www/mesh/about/index.tsx
@@ -0,0 +1,10 @@
+export const Name = "x-Driven Engineering Team";
+export const Acronym = "xDE";
+
+export const Hashtags = [Acronym, "xDETeam"];
+
+export const GitHub = "https://github.com/XDETeam";
+
+export const SocialLinks = {
+    github: GitHub
+};
