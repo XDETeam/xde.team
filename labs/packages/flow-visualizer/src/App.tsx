5e7e75e358b44f7fd67f1cab14ba8a63e870ddc6
yavulan <yavulan@gmail.com>
2020-10-09 11:46:14 +0300

    +Express webapp visualization

--- labs/packages/flow-visualizer/src/App.tsx
+++ labs/packages/flow-visualizer/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { webapp } from "@xde/flow-manager";
+import { root } from "@xde/webapp-express/dist/functors/app";
 // import { root } from "@xde/flow-manager/.build/lab-business";
 
 import MeshComponent from "./components/MeshComponent";
 
 function App() {
-	return <MeshComponent functor={webapp} />;
+	return <MeshComponent functor={root} />;
 }
 
 export default App;
