1781bb3899450f53097e720a6c4eee618ed2727d
yavulan <yavulan@gmail.com>
2020-10-12 10:50:17 +0300

    *Minor

--- labs/flow/flow-visualizer/src/App.tsx
+++ labs/flow/flow-visualizer/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { webapp } from "@xde/flow-manager";
-// import { root } from "@xde/webapp-express/dist/functors/app";
-import { root } from "@xde/lab-business";
+import { webapp } from "@xde/flow-manager/.build/webapp";
+import { root } from "@xde/webapp-express/dist/functors/app.js";
+// import { root } from "@xde/lab-business";
 
 import MeshComponent from "./components/MeshComponent";
 
