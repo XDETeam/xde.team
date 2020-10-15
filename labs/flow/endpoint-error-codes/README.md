a1709b80cb457451cf20de336590bbd82ede8d5b
yavulan <yavulan@gmail.com>
2020-10-15 15:56:29 +0300

    +Endpoint-error-codes

---
+++ labs/flow/endpoint-error-codes/README.md
@@ -0,0 +1,2 @@
+I'm exists to be able to respond with simple number.
+And client will verbose/translate/etc. this number.
---
+++ labs/flow/endpoint-error-codes/src/index.ts
@@ -0,0 +1,7 @@
+export enum EndpointErrorCodes {
+	InsecureApiRequest,
+	WrongCredentials,
+	InternalServerError,
+	MethodNotAllowed,
+	UnprocessableEntity,
+}
