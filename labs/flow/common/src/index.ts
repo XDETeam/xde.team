e52acded382c28095ab28245382891c66a9d2c14
yavulan <yavulan@gmail.com>
2020-10-30 12:22:00 +0200

    +Some types
    *JSON circular

--- labs/flow/common/src/index.ts
+++ labs/flow/common/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./http";
 export * from "./node";
+export * from "./json";
 export * from "./types";
 export * from "./endpoint";
 export * from "./request";
--- labs/flow/common/src/types/index.ts
+++ labs/flow/common/src/types/index.ts
@@ -1,3 +1,9 @@
 export interface Constructable<T> {
 	new (...args: any): T;
 }
+
+export type TUndefined<T> = Record<keyof T, undefined>;
+
+export interface IDictionary<T = any> {
+	[key: string]: T;
+}
