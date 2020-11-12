773f7cab01a65338cc07a01ffa511bdf70879ab8
yavulan <yavulan@gmail.com>
2020-11-12 21:04:52 +0200

    -Added missing files

--- labs/flow/aspects/src/todo/GeneratedApiBody.ts
+++ labs/flow/aspects/src/todo/GeneratedApiBody.ts
@@ -1,5 +1,7 @@
+import { TCommonApiResponse } from "@xde/common";
+
 export const GeneratedApiBody = "GeneratedApiBody" as const;
 
 export type TGeneratedApiBody = {
-	[GeneratedApiBody]: string;
+	[GeneratedApiBody]: TCommonApiResponse;
 };
--- labs/flow/endpoint-error-codes/src/index.ts
+++ labs/flow/endpoint-error-codes/src/index.ts
@@ -1,4 +1,4 @@
-export enum EndpointErrorCodes {
+export enum EndpointErrorCode {
 	InsecureApiRequest,
 	WrongCredentials,
 	InternalServerError,
