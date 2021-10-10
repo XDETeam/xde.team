fd51301d5197d57afca8dcec5312cc1eb2b15a30
stan <stanislav@yarmonov.pro>
2021-10-10 10:51:03 +0300

    +Solution boilerplate

---
+++ src/Xde.Team.Specs/NamespaceDoc.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Xde.Team.Specs
+{
+    /// <summary>
+    /// TODO:
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+
+    }
+}
---
+++ src/Xde.Team.Specs/Program.cs
@@ -0,0 +1,8 @@
+﻿// TODO:Fill application properties (AssemblyVersion, AssemblyFileVersion,
+// AssemblyInformationalVersion, Copyright, Description, etc). Share some
+// of the between assemblies using some Xde.Meta package or Xde.Specs (if
+// simply shared file is linked).
+// TODO:Choose versioning model
+var appName = typeof(Xde.Team.Specs.NamespaceDoc).Assembly.GetName();
+
+Console.WriteLine($"Team Server {appName.Version}");
