4f2791dde6f7236bc44b75ea649340100703e398
stan <stanislav@yarmonov.pro>
2021-10-16 18:59:27 +0300

    +Multiple web apps experiment

--- src/Xde.Team.Specs/Program.cs
+++ src/Xde.Team.Specs/Program.cs
@@ -3,6 +3,9 @@
 // of the between assemblies using some Xde.Meta package or Xde.Specs (if
 // simply shared file is linked).
 // TODO:Choose versioning model
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,7 +17,7 @@ using Xde.Host;
 
 var builder = Host
     .CreateDefaultBuilder(args)
-    .UseEnvironment(Environments.Development) // TODO:
+    .UseEnvironment(Environments.Development) //TODO:
     .ConfigureServices(services => services
         .AddHostedService<CliHostedService>()
     )
@@ -26,6 +29,32 @@ if (args is { Length: > 0 })
         => configuration.AddCommandLine(args)
     );
 }
+
+// TODO: Multiple web host do not work simultaneously
+// TODO:Dashboard and any other webapp can be optional, so WebApplication.CreateBuilder
+// looks not good
+// TODO:Settings (like endpoints should be also configurable)
+builder
+    .ConfigureWebHostDefaults(dashboard => dashboard
+        .UseUrls("https://localhost:4001")
+        .Configure(application => application
+            .UseRouting()
+            .UseEndpoints(endpoints => endpoints
+                .MapGet("/", () => "Team server dashboard")
+            )
+        )
+    )
+
+    .ConfigureWebHostDefaults(proxy => proxy
+        .UseUrls("https://localhost:4002")
+        .Configure(application => application
+            .UseRouting()
+            .UseEndpoints(endpoints => endpoints
+                .MapGet("/", () => "Team server proxy service")
+            )
+        )
+    )
+;
 
 var host = builder.Build();
 
