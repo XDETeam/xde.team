a6db3a8b3bb3a624f529c86e0ae4e32d833467d6
stan <stanislav@yarmonov.pro>
2021-10-19 21:38:18 +0300

    +Speflow and simple boilerplate

---
+++ src/Xde.Team.Specs/Mesh/FlowBasics.feature
@@ -0,0 +1,11 @@
+﻿Feature: TODO: FlowBasics
+
+A short summary of the feature
+
+Scenario: Get flows by tags
+	Given Default flows
+	And User Stan
+	
+	When Get flows by tags xde,server
+
+	Then Display flow "XDE Team Server"
---
+++ src/Xde.Team.Specs/Mesh/FlowSteps.cs
@@ -0,0 +1,32 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Xde.Mesh
+{
+    [Binding]
+    public class FlowSteps
+    {
+        [Given(@"Default flows")]
+        public void GivenDefaultFlows()
+        {
+            //TODO: throw new PendingStepException();
+        }
+
+        [Given(@"User (.+)")]
+        public void GivenUserStan(string userName)
+        {
+            //TODO: throw new PendingStepException();
+        }
+
+        [When(@"Get flows by tags (.+)")]
+        public void WhenGetFlowsByTagsXdeServer(string tags)
+        {
+            //TODO: throw new PendingStepException();
+        }
+
+        [Then(@"Display flow ""(.+)""")]
+        public void ThenDisplayFlow(string topic)
+        {
+            //TODO: throw new PendingStepException();
+        }
+    }
+}
