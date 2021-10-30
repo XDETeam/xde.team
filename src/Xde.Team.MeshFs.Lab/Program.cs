91efce4f1e80f9a1bd76727a526be2d99661d4c2
stan <stanislav@yarmonov.pro>
2021-10-30 14:43:11 +0300

    +MeshFs provider boilerplate

--- src/Xde.Team.MeshFs.Lab/Program.cs
+++ src/Xde.Team.MeshFs.Lab/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Xde.Lab.MeshFs;
+
+var builder = WebApplication.CreateBuilder();
+
+builder
+    .Services
+    .AddHostedService<MeshFsProvider>()
+;
+
+var host = builder
+    .Build()
+;
+
+host.Run();
