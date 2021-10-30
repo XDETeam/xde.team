f297e5cea8f545a664f27256b7eceff3dbe5035f
stan <stanislav@yarmonov.pro>
2021-10-30 19:12:07 +0300

    +Basic options

---
+++ src/Xde.Team.MeshFs.Lab/MeshFsOptions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Xde.Lab.MeshFs;
+
+public record MeshFsOptions
+{
+    public const string Section = "MeshFs";
+    public const string DefaultWebDavRoute = "/webdav";
+
+    public string WebDavRoute { get; set; } = DefaultWebDavRoute;
+
+    internal class Setup
+        : IConfigureOptions<MeshFsOptions>
+    {
+        private readonly IConfigurationSection _options;
+
+        public Setup(IConfiguration config)
+        {
+            _options = config.GetSection(Section);
+        }
+
+        void IConfigureOptions<MeshFsOptions>.Configure(MeshFsOptions options)
+        {
+            _options?.Bind(options);
+        }
+    }
+}
