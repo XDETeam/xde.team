d3a9a1fa639ad94a1d7b294d254f15afa981528f
stan <stanislav@yarmonov.pro>
2021-12-19 21:35:28 +0200

    +WebDAV controller experiments

---
+++ src/Xde.Team.MeshFs.Lab/WebDavController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Xde.Lab.MeshFs
+{
+    public class WebDavController
+        : ControllerBase
+    {
+        [WebDavPropfind]
+        public async Task<ActionResult<string>> List(string path)
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<string>> Index(string path)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/Xde.Team.MeshFs.Lab/WebDavPropfindAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace Xde.Lab.MeshFs
+{
+    public class WebDavPropfindAttribute
+        : HttpMethodAttribute
+    {
+        public const string MethodName = "PROPFIND";
+
+        public WebDavPropfindAttribute()
+            : base(Enumerable.Repeat(MethodName, 1))
+        {
+
+        }
+    }
+}
