f2184fcf205b81bcb6ba986e144dd1087b490e29
Stanislav S. Yarmonov <stanislav@yarmonov.pro>
2022-08-25 10:52:39 +0300

    +Labs

---
+++ www/pages/labs/index.tsx
@@ -0,0 +1,46 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+export const ShortName = "Labs";
+export const FullName = "XDE Team Labs";
+
+const Page: NextPage = () => {
+    return (
+        <div>
+            <Head>
+                <title>{FullName}</title>
+            </Head>
+
+            <main>
+                <h1>
+                    {FullName}
+                </h1>
+
+                <ul>
+                    <li>
+                        Потоко/событийно-ориентированное программирование. Flow.
+                    </li>
+                    <li>
+                        Организаций тестов. На примере имеющихся набросков ISpecification,
+                        многослойных тестов, c DI/IoC и т.п.
+                    </li>
+                    <li>
+                        DI/IoC. Особенно интересует композиция, роутинг, подходы в разных языках,
+                        пример с factorial+memoization.
+                    </li>
+                    <li>
+                        Kubernetes formula.
+                    </li>
+                    <li>
+                        Rust formula.
+                    </li>
+                    <li>
+                        SOLID в базах данных. Хранимые процедуры и представления.
+                    </li>
+                </ul>
+            </main>
+        </div>
+    )
+}
+
+export default Page
