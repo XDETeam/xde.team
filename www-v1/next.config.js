2eece014b3efb7fefbd7c1ad30b4e8ab4a77aef6
Stanislav S. Yarmonov <stanislav@yarmonov.pro>
2023-01-29 20:00:50 +0200

    *Localization

--- www-v1/next.config.js
+++ www-v1/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	swcMinify: true,
+
+	pageExtensions: ["mdx", "md", "page.jsx", "page.js", "page.tsx", "page.ts"],
+	
+	i18n: {
+		locales: ["en", "ru", "bg"],
+		defaultLocale: "en",
+	},
 }
 
 module.exports = nextConfig
--- www-v1/pages/_document.page.tsx
+++ www-v1/pages/_document.page.tsx
@@ -1,8 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { useRouter } from 'next/router';
 
 export default function Document() {
+  const router = useRouter();
+  const { locale } = router;
+    
   return (
-    <Html lang="en">
+    <Html lang={ locale }>
       <Head />
       <body>
         <Main />
