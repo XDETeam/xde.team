4013cbf7a967fc8315ff5350330c4e31fc495536
Stanislav S. Yarmonov <stanislav@yarmonov.pro>
2023-03-09 20:16:32 +0200

    *Deployment

--- www/deploy.sh
+++ www/deploy.sh
@@ -1,6 +1,4 @@
 cd /projects/xde/xde.team/www
 rsync -avz --delete ./.next/standalone/ dc.hetzner1.app1:/data/volumes/team_xde/www
-rsync -avz --delete ./.next/static dc.hetzner1.app1:/data/volumes/team_xde/www/.next
-rsync -avz --delete ./package.json dc.hetzner1.app1:/data/volumes/team_xde/www/package.json
-rsync -avz --delete ../node_modules dc.hetzner1.app1:/data/volumes/team_xde/www
+rsync -avz --delete ./.next/static dc.hetzner1.app1:/data/volumes/team_xde/www/www/.next
 ssh dc.hetzner1.app1 docker restart team_xde
